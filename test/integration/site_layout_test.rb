cded2f8f473003031d4e09209ca35d82ad3ab84f
anhkt <anhkt1603@gmail.com>
2016-10-18 19:45:37 +0700

    finish-chap6

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,12 +5,12 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template "static_pages/home"
-    assert_select "a[href=root]", root_path, count: 2
+    assert_select "a[href=home]", root_path, count: 2
     assert_select "a[href=help]", help_path
     assert_select "a[href=about]", about_path
     assert_select "a[href=contact]", contact_path
     get contact_path
-    assert_select "title", full_title "Contact"
+    assert_select "title", full_title: "Contact"
   end
 
 end
